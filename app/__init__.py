68ef6dd42eb537326ce7e562422ddb887b803d2d
Ben Dowdell <ben.dowdell87@gmail.com>
2021-02-27 17:07:06 -0600

    updates for SonicAI project

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright YYYY, FN MI LN
+Copyright 2021, Benjamin L. Dowdell
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -19,8 +19,14 @@ limitations under the License.
 from flask import Flask
 from flask_mail import Mail
 from config import config
+import joblib
 
 mail = Mail()
+
+# load the models using joblib and make them available to the app
+xgb_model = joblib.load('app/api/static/models/xgbr_fitted.joblib')
+pcr_model = joblib.load('app/api/static/models/pcr_fitted.joblib')
+knn_model = joblib.load('app/api/static/models/knn_fitted.joblib')
 
 
 def create_app(config_name):
@@ -30,5 +36,8 @@ def create_app(config_name):
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix='/api')
     
     return app
