b1b310333eba2a9cedfec8670f1b35d35bb6af92
Ben Dowdell <ben.dowdell87@gmail.com>
2021-02-28 13:24:17 -0600

    udpates boilerplate notice

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-Copyright YYYY, FN MI LN
+Copyright 2021, Benjamin L. Dowdell
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
